2145bcc02af7dc6ba5217836999d790883a16a3e
takoakie <64633967+takoakie@users.noreply.github.com>
2020-11-16 11:13:14 +0900

    20201116 hooter,header,buttonを加えました

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,21 @@
   <div id="app">
     <img alt="./assets/favicon.png" src="./assets/Scarecrow.png">
     <HelloWorld msg="I have bad news for you"/>
+  
+  <button class="button"> BACK TO HOMEPAGE </button>
   </div>
-
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
-export default {
+export default{
+  data: function(){
+  return{
+    header: "404 NOT FOUND",
+    footer:"Your name @ DevChallenge.io"
+  };
+},
   name: 'App',
   components: {
     HelloWorld
